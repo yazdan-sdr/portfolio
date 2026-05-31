2875bbb8e653aa4a918330e1be5f5906427fcc2f
Yazdan Sadri <yazdansdr@gmail.com>
2026-06-01 02:10:42 +0330

    folders organized

---
+++ app/(root)/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+    title: "Home",
+};
+
+const Homepage = () => {
+    return (
+        <div>
+            <h1>Welcome to the Homepage</h1>
+        </div>
+    );
+};
+
+export default Homepage;
---
+++ lib/constant/index.tsx
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "yazdan";
+export const APP_DESCRIPTION =
+    "A Portfolio website, created by yazdan sadri, using latest technologies to show different projects.";
+
+export const APP_SERVER_URL =
+    process.env.NEXT_PUBLIC_APP_SERVER_URL || "http://localhost:3000/";
